982295249b6bc45c7368831c4feceb60c6ff7809
Marcelo Feliciano <feliciano@keemail.me>
2026-05-23 22:38:58 -0300

    feat: update development environment configuration and rename Mock API

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,10 @@
 import { ApiConfig } from '../app/api/config/api.config';
 
+// Use localhost:8080 for local development or localhost:3000/dev for Mockoon
 export const environment = {
   production: false,
   apiConfig: {
-    baseUrl: 'http://localhost:3000/dev'
+    baseUrl: 'http://localhost:8080'
   } as ApiConfig
 };
 
